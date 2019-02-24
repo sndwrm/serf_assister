a2cb1979112557a4fc7c57cd4af38debf059bd8c
sndwrm <21321466+sndwrm@users.noreply.github.com>
2019-02-24 04:49:36 +0300

    various API data

--- serf_assister_end.user.js
+++ serf_assister_end.user.js
@@ -1,11 +1,15 @@
 // ==UserScript==
-// @name        serf_assister_end
-// @namespace   Anonymous
-// @description Hide, remove, replace, rearrange and modify various elements on various sites.
-// @version     1
-// @include *
-// @require https://code.jquery.com/jquery-3.3.1.slim.min.js
-// @license WTFPL
+// @name                serf_assister_end
+// @namespace           serf_assister
+// @description         Hide, remove, replace, rearrange and modify various elements on various sites.
+// @version             1.0.0.1
+// @homepageURL         https://github.com/sndwrm/serf_assister
+// @run-at              document-start
+// @match               *
+// @noframes
+// @run-at              document-end
+// @inject-into         page
+// @license             WTFPL
 // ==/UserScript==
 
 const DEBUG_TYPE = true;//true = log, dumps DOM in html. false = dir, dumps DOM in json.
@@ -15,6 +19,8 @@ const CURRENT_HOST = window.location.hostname;
 const MANAGED_HOSTS = ['www.xvideos.com', 'mail.zoho.com', 'aliexpress.com'];
 const SCRIPT_VERSION = "1.0.0.1";
 const QUOTES_TYPE = "double";//"SINGLE". Quotes to use when cleaning string.
+const CURRENT_WINDOW = unsafeWindow;
+
 
 console.log(DEBUG_MARK + "Loaded script v." + SCRIPT_VERSION);
 
@@ -94,8 +100,74 @@ function manage_host(host_url) {
     }
 }
 
+CURRENT_WINDOW.document.addEventListener('keydown', function (e) {
+    e = e || window.event;
+    if (e.keyCode === 13 && e.ctrlKey && !e.shiftKey && !e.altKey) e.stopImmediatePropagation();
+}, true);
+
 if (host_among_managed(CURRENT_HOST)) debug('Current host = \"' + CURRENT_HOST + '\" is among managed');
 else debug('Current host = \"' + CURRENT_HOST + '\" is NOT among managed, passing...');
+
+debug("HOST URL from extension: " + extension.getURL());
+debug("inIncognitoContext: " + extension.inIncognitoContext);
+debug("extension.isAllowedFileSchemeAccess: " + extension.isAllowedFileSchemeAccess());
+// browser.runtime.sendMessage()
+// browser.runtime.onMessage
+
+
+// For example, here's a content script which listens for click events in the web page. If the click was on a link, it
+// messages the background page with the target URL: window.addEventListener("click", notifyExtension);  function
+// notifyExtension(e) { if (e.target.tagName != "A") { return; } browser.runtime.sendMessage({"url": e.target.href}); }
+
+// The background script listens for these messages and displays a notification using the notifications API:
+// browser.runtime.onMessage.addListener(notify);
+//
+// function notify(message) {
+//     browser.notifications.create({
+//         "type": "basic",
+//         "iconUrl": browser.extension.getURL("link.png"),
+//         "title": "You clicked a link!",
+//         "message": message.url
+//     });
+// }
+
+// If you have multiple content scripts communicating at the same time, you might want to store each connection in an
+// array. // background-script.js  var ports = []  function connected(p) { ports[p.sender.tab.id]    = p //... }
+// browser.runtime.onConnect.addListener(connected)  browser.browserAction.onClicked.addListener(function() {
+// ports.forEach(p => { p.postMessage({greeting: "they clicked the button!"}) }) });
+
+
+// menus.getTargetElement() - Returns the element for a given targetElementId. Only works in the document where the
+// right clicked element is in, so everywhere but in the background page. Syntax let elem =
+// browser.menus.getTargetElement(targetElementId); Parameters targetElementId - The property of the menus.OnClickData
+// object passed to the menus.onClicked handler or menus.onShown event. Return value - The element referred to by the
+// targetElementId parameter. If the targetElementId parameter is not valid, the method returns null. The following
+// example uses the getTargetElement method to get the element referred to by the info.targetElementId property and
+// then removes it.  browser.menus.create({ title: "Remove element", documentUrlPatterns: ["*://*/*"], contexts:
+// ["audio", "editable", "frame", "image", "link", "page", "password", "video"], onclick(info, tab) {
+// browser.tabs.executeScript(tab.id, { frameId: info.frameId, code:
+// `browser.menus.getTargetElement(${info.targetElementId}).remove();`, }); }, });
+
+
+// i18n.getMessage() - Gets the localized string for the specified message.
+// Syntax
+// browser.i18n.getMessage(
+//     messageName,  // string
+//     substitutions // optional any
+// )
+// Parameters
+// messageName - string. The name of the message, as specified in the messages.json file. If the message can't be found
+// in messages.json: Firefox returns "" and logs an error. Chrome returns "" and does not log an error.
+// substitutionsOptional - string or array of string. A single substitution string, or an array of substitution
+// strings. In Chrome, if you supply more than 9 substitution strings, getMessage() will return undefined. Return value
+// string. Message localized for current locale. Examples Get the localized string for "messageContent", with
+// target.url substituted:  var message = browser.i18n.getMessage("messageContent", target.url); console.log(message);
+// This would work with a _locales/en/messages.json file containing: { "messageContent": { "message": "You clicked
+// $URL$.", "description": "Tells the user which link they clicked.", "placeholders": { "url" : { "content" : "$1",
+// "example" : "https://developer.mozilla.org" } } } }  If target.url is "https://developer.mozilla.org", then the
+// value of message, in the "en" locale, would be: "You clicked https://developer.mozilla.org."
+
+
 
 // manage_host(CURRENT_HOST);
 
@@ -115,3 +187,39 @@ else debug('Current host = \"' + CURRENT_HOST + '\" is NOT among managed, passin
 //  When adding events, they are stacking one on another, means you can have more than one
 //  click event on same element, so they not overwrite eachother. You must use
 //  removeEventListener to first remove previous event and only then assign new one
+
+
+// @match               *://*/*
+
+// @resource text https://my.cdn.com/some-text.txt
+
+// @run-at - Decide when the script will execute.
+// document-end - The script executes when DOMContentLoaded is fired. At this time, the basic HTML of the page is ready
+// and other resources like images might still be on the way. document-start - The script executes as soon as possible.
+// There is no guarantee for the script to execute before other scripts in the page. Note: in Greasemonkey v3, the
+// script may be ensured to execute even before HTML is loaded, but this is impossible for Violentmonkey as a web
+// extension. document-idle - The script executes after DOMContentLoaded is fired.
+
+// @inject-into - Decide which context the script will be injected into. If not set in the metadata block, the default
+// value page will be used. However, you can change the default value in Violentmonkey settings. page - Inject into
+// context of the web page. In this mode, unsafeWindow refers to the window object, allowing the script to access
+// JavaScript objects of the web page, just like normal page scripts can. content - Inject into context of content
+// scripts. In this mode, unsafeWindow refers to the global object in content script. As a result, the script can
+// access and modify the page's DOM, but cannot access JavaScript objects of the web page. auto - Try to inject into
+// context of the web page. If blocked by CSP rules, inject as a content script.
+
+// @require             https://code.jquery.com/jquery-3.3.1.slim.min.js
+
+// Values
+// GM.deleteValue - Deletes a value from chrome that was previously set.
+// GM.getValue - Retrieves stored values, see GM.setValue below.
+// GM.listValues - Retrieves an array of stored values' keys.
+// GM.setValue - Permanently stores a value under a key, later available via GM.getValue.
+// Resources
+// GM.getResourceUrl - Provides a URL for loading a @resource, such as an image, into the page.
+// Other
+// GM.notification - Opens a notification dialog.
+// GM.openInTab - Opens a given URL in a new tab.
+// GM.setClipboard - Sets the contents of the clipboard.
+// GM.xmlHttpRequest - A variant of XMLHttpRequest, this method allows skipping use the same-origin policy, enabling
+// complex mashups.
